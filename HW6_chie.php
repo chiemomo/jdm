b5c4b68e710e655f4e03b41438fd751658b85144
chiemomo <chiemomo@gmail.com>
2014-04-25 06:57:20 -0700

    update

--- HW6_chie.php
+++ HW6_chie.php
@@ -161,12 +161,12 @@ function load_product_details()
 				}
 					
 				//Check for empty values
-				/*if(customer == '' || email == '' || club == '' || shaft == '' || quantity == '')
+				if(customer == '' || email == '' || club == '' || shaft == '' || quantity == '')
 				{
 					//show the html error message where div.error if there is empty field
-					$('.error').fadeIn(400).show().html('Please fill required fields.'); 
+					$('.error').fadeIn(400).show().html('<p class="red">*Please fill out all required fields.</p>'); 
 				}
-				else*/
+				else
 				{
 					//construct the data string to insert the table	
 					var datastring = "customer=" + customer + "&email=" + email + "&club=" + club + "&shaft=" + shaft + "&quantity=" + quantity 
@@ -190,10 +190,11 @@ function load_product_details()
 								//$('.success').fadeIn(2000).show().html('Thanks ' +customer + ', your request has been submitted successfully!').fadeOut(6000); //Show, then hide success msg
 								$('.error').fadeOut(2000).hide(); //If showing error, fade out
 								
-								var thanks = "Thank you, " + customer + " for your interest!<br>"
-								thanks += "Your quote for " + club + " with " + shaft + " shaft is on the way." 
+								var thanks = "<p>Thanks " + customer + " for your interest!</p>"
+								thanks += "<p>Your quote for " + club + " with " + shaft + " shaft is on its way.</p>" 
 								
 								$(function() {
+									$( "#success" ).dialog({ position: { my: "center", at: "center", of: "#form_wrapper" } });
 									$( "#success" ).dialog().html(thanks);
 								});
 								
@@ -253,25 +254,27 @@ function load_product_details()
 			<div class="container_right">
 				<!-- quote form -->
 				<div id="form_wrapper">
+					<h2>Get A Quote Now!</h2>
+					<p><small>* required field</small></p>
 					<form method="post" name="form" id="quote_form">
 						<!-- jquery ui auto-complete for club -->
 						<div class="ui-widget">
-							<label for="club">Clubs: </label>
-							<input id="club">
+						<label for="club">Clubs*: </label>
+						<input id="club">
 						</div>
 						<!-- jquery ui auto-complete for shaft -->
 						<div class="ui-widget">
-							<label for="shaft">Shafts: </label>
-							<input id="shaft">
+						<label for="shaft">Shafts*: </label>
+						<input id="shaft">
 						</div>
-						<p><label>Quantity : </label>
+						<p><label>Quantity*: </label>
 						<input type="text" id="quantity" name="quantity" /></p>
-						<p><label>Full Name: </label>
+						<p><label>Full Name*: </label>
 						<input type="text" id="customer" name="customer" /></p>
-						<p><label>Email Address: </label>
+						<p><label>Email Address*: </label>
 						<input type="text" id="email" name="email" /></p>
 						<p><label>Comment: </label>
-						<textarea name="text" id="comment" rows="5" cols="35"></textarea></p>
+						<textarea name="text" id="comment" rows="3" cols="34"></textarea></p>
 						<input type="checkbox" id="subscribe" name="subscribe" value="yes" checked>Subscribe Newsletter
 						<p><button type="submit" class="submit" value="submit">Get A Quote!</button></p>
 					</form>
