10f7361ec61dae4211e3baefb5e3d622b8d1bb1f
chiemomo <chiemomo@gmail.com>
2014-04-25 02:20:53 -0700

    hhw6

--- generate_shaft.php
+++ generate_shaft.php
@@ -1,22 +1,27 @@
 <?php
-
 require 'includes/constants/dbc.php';
 
-//A functions which return an array holding all club in the product table
+//PREPARE A SHAFT LIST IN JSON FORMAT fOR JQUERY UI AUTO-COMPLETION
+
+//A functions which return an array holding all shaft in the shaft table
 function get_shaft(){
 
-	$query="SELECT shaft FROM " . TABLE_SHAFTS . " WHERE 1;";
+	//pull shaft data from the shaft table with ascending order
+	$query="SELECT shaft FROM " . TABLE_SHAFTS . " ORDER BY shaft ASC;";
 	$result = mysql_query($query) or die(mysql_error());
 	
+	//define an array to hold shaft data
 	$shaft = array();
 	
 	while (true)
 	{
 		$row = mysql_fetch_array($result);
 		
+		//when there is no more row, break the while loop
 		if (!$row)
 			break;
 		
+		//sanitize the string
 		$shaft[] = str_replace("\r","",$row['shaft']);
 	}
 	
@@ -24,6 +29,7 @@ function get_shaft(){
 
 }
 
+//execute the function and convert the array to json format
 $autocomplete_list = get_shaft();
 $response = array("shaft" => $autocomplete_list);
 echo json_encode($response);
